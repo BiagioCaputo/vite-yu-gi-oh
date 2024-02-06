798f34521f8fb3f73d018f0358ef975d0f5af6d7
BiagioCaputo <scebiagio98@gmail.com>
2024-02-06 15:29:11 +0100

    created select with value and text dinamics

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,41 @@
 <script>
 export default {
     name: 'AppHeader',
+    data() {
+        return {
+            types: [
+                "Bug",
+                "Dark",
+                "Dragon",
+                "Electric",
+                "Fairy",
+                "Fighting",
+                "Fire",
+                "Flying",
+                "Ghost",
+                "Grass",
+                "Ground",
+                "Ice",
+                "Normal",
+                "Poison",
+                "Psychic",
+                "Rock",
+                "Steel",
+                "Water"
+            ]
+        }
+    }
 
 };
 </script>
 
 <template>
     <header>
+        <select name="type">
+            <option value="all">All</option>
+            <option v-for="(type, index) in this.types" :value="type">{{ type }}</option>
+        </select>
+
     </header>
 </template>
 
